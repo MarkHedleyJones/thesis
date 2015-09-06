84653eecefb4c1c56f1b9d4bfefc886bd9e6fb2d
Mark H. Jones <markjones112358@gmail.com>
2015-09-06 19:14:57 +1200

    Revised contents page

--- compile.sh
+++
@@ -1,6 +0,0 @@
-pdflatex -draftmode thesis
-bibtex thesis # or biber
-makeindex thesis.idx # if needed
-#makeindex -s style.gls ...# for glossary if needed
-pdflatex -draftmode thesis.tex
-pdflatex thesis
